dbb3aa508c66c4e9229f60e7fda23ccb78e3dd6d
SumitMaran2 <75078138+SumitMaran2@users.noreply.github.com>
2021-01-10 17:23:27 +0530

    Add files via upload

---
+++ Box3.js
@@ -0,0 +1,38 @@
+class Box3{
+    constructor(x, y, width, height){
+    var options = {
+    isStatic:false
+    }
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.Visiblity = 255;
+    this.body = Bodies.rectangle(x,y,this.width,this.height, options)
+    World.add(world, this.body);
+    }
+    score(){
+        if(this.Visiblity<0 && this.Visiblity>(-105)){
+          score++;
+        }
+        }
+    display(){
+    if(this.body.speed < 3){ 
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y)
+    rectMode(CENTER);
+    fill(255,192,203);
+    rect(0,0,this.width, this.height)
+    pop();
+    }else{
+        World.remove(world, this.body);
+            push();
+            this.Visibility = this.Visibility - 5;
+            tint(255,this.Visiblity);
+            pop();
+    }
+    }
+    
+    }
+    
